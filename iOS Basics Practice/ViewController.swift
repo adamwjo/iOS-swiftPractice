61e730cd29844cd769ee1e8bc25b6969761146ba
adamwjo <adamwjo@gmail.com>
2019-03-20 21:06:13 -0400

    finished

--- iOS Basics Practice/ViewController.swift
+++ iOS Basics Practice/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
